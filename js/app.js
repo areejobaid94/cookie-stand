d00a5ec73ba2e0b7b75d8043d56390a9e366905c
areejobaid94 <areej.obaid@yahoo.com>
2021-01-05 17:01:58 +0200

    fix css

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 'use strict';
-
+let count = 6;
 let timeArray = ['Time', '6am', '7am', '8am', '9am', '10am', '11am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'Total']
-let tableHeads = ['Location', 'Min / Cust', 'Max / Cust', 'Avg Cookie / Sale'];
+let tableHeads = ['Location', 'Addresse','Min / Cust', 'Max / Cust', 'Avg Cookie / Sale','Hours Open' , 'Contact Information'];
 let storesArrey = [];
 let totalValuePerHour = ['Totals'];
 let totalSalmonCookieTossersPerHour = ['Totals'];
-
 let hourlyCustomerTraffic = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6]
 
 let printTableHeader = function (table, values) {
@@ -49,13 +48,16 @@ let printTableRow = function (table, values, location) {
     table.appendChild(row);
 }
 
-function StoreObj(location, min, max, average) {
+function StoreObj(location, min, max, average,addresse,hoursOpen,contactInformation) {
     this.location = location;
+    this.addresse = addresse;
     this.min = min;
     this.max = max;
     this.average = average;
     this.totalCooliesPerHour = [];
     this.totalSalmonCookieTossers = []; 
+    this.hoursOpen = hoursOpen;
+    this.contactInformation = contactInformation;
     this.calculations();
 }
 
@@ -105,11 +107,11 @@ function startPage() {
     let tableManageStaffing = document.getElementById('manage-staffing');
     printTableHeader(tableManageStaffing, timeArray);
 
-    storesArrey.push(new StoreObj('Seattle', 23, 65, 6.3));
-    storesArrey.push(new StoreObj('Tokyo', 3, 24, 1.2));
-    storesArrey.push(new StoreObj('Dubai', 11, 38, 3.7));
-    storesArrey.push(new StoreObj('Paris', 20, 38, 2.3));
-    storesArrey.push(new StoreObj('Lima', 2, 16, 4.6));
+    storesArrey.push(new StoreObj('Seattle', 23, 65, 6.3,'site number: 4','6am - 7pm' , 'Email: areej.obaid@yahoo.com'));
+    storesArrey.push(new StoreObj('Tokyo',3, 24, 1.2,'site number: 5','6am - 7pm' ,'Email: areej.obaid@yahoo.com' ));
+    storesArrey.push(new StoreObj('Dubai',11, 38, 3.7,'site number: 17' ,'6am - 7pm' , 'Email: areej.obaid@yahoo.com'));
+    storesArrey.push(new StoreObj('Paris' ,20, 38, 2.3,'site number: 19','6am - 7pm' , 'Email: areej.obaid@yahoo.com'));
+    storesArrey.push(new StoreObj('Lima' ,2, 16, 4.6, 'site number: 1','6am - 7pm' , 'Email: areej.obaid@yahoo.com'));
 
     for (let i = 0; i < storesArrey.length; i++) {
         printTableRow(htmlTable, storesArrey[i]);
@@ -122,3 +124,13 @@ function startPage() {
     table.appendChild(htmlTable);
 }
 startPage();
+
+function changeImg(){
+    console.log('hi');
+    let imgToHide = 'img-main-'+ count;
+    count > 1 ?  count = count - 1 : count = 6;
+    let imgToShow = 'img-main-' + count;
+
+    document.getElementById(imgToHide).style.display = 'none';
+    document.getElementById(imgToShow).style.display = 'block';
+}
